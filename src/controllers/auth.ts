af950f1ebe6b1f37fbf2d161319263f6f66840f6
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-01-28 06:46:12 +0100

    delete unused imports

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+
 import { loginService, registerUserService } from "../services/auth.js";
 
 const registerController = async (req: Request, res: Response) => {
