fe96624d5be6b306bc447d1897a9ddb4e4a3e850
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-01-28 03:40:51 +0100

    update route import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
+import authRoutes from './routes/auth.js';
 // import productRoutes from './routes/productRoutes';
 // import { authenticate } from './middlewares/authMiddleware';
 
